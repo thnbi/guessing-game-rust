4b220c46eda0e298890da672c6eb9f86dba3ad88
thnbi <renato_freitas13@icloud.com>
2023-02-02 18:29:48 -0300

    loop for atempts and guess validation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
 
     println!("the secret number is: {secret_number}");
 
-    println!("please input your guess");
+    loop {
+        println!("please input your guess");
 
-    let mut guess = String::new();
+        let mut guess:String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed  to read line");
 
-    println!("you guessed: {guess}")
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
 }
